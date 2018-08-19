1eb5f60e694178441061491abf8bf1b3df268802
kobatako <ju.koba.itit.shar@gmail.com>
2018-08-19 18:33:18 +0900

    feature hello world

--- src/bloom_app.erl
+++ src/bloom_app.erl
@@ -15,6 +15,14 @@
 %%====================================================================
 
 start(_StartType, _StartArgs) ->
+    Dispatch = cowboy_router:compile([
+        {'_', [
+            {"/", toppage_handler, []}
+        ]}
+    ]),
+    {ok, _} = cowboy:start_clear(http, [{port, 8010}], #{
+        env => #{dispatch => Dispatch}
+    }),
     bloom_sup:start_link().
 
 %%--------------------------------------------------------------------
